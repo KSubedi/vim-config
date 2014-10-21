77aad9a5533fdf85abe8bd2298654e62c35e0cbe
Kaushal Subedi <kaushal@iscene.wp.gy>
2014-10-20 23:57:53 -0700

    Updated updater script

--- update.sh
+++ update.sh
@@ -1,4 +1,7 @@
 #!/bin/bash          
+git config --global user.name "Kaushal Subedi"
+git config --global user.email kaushal@wireshock.com
+cd ~
+git clone https://github.com/KSubedi/vim-config.git
+mv vim-config .vim
 ln -s ~/.vim/.vimrc ~/.vimrc
-
-
