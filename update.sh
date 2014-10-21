c414cf4ef544d88a4b6104b9da57e545c1b7fd43
Kaushal Subedi <kaushal@wireshock.com>
2014-10-21 01:18:18 -0700

    Updated updater script to clone git repos for bundles

--- update.sh
+++ update.sh
@@ -4,3 +4,12 @@ git config --global user.email kaushal@wireshock.com
 cd ~
 git clone https://github.com/KSubedi/vim-config.git
 mv vim-config .vim
+cd .vim
+mkdir bundle
+cd bundle
+git clone https://github.com/Shougo/neobundle.vim
+git clone https://github.com/Shougo/neocomplcache.vim
+git clone https://github.com/scrooloose/syntastic
+git clone https://github.com/bling/vim-airline
+git clone https://github.com/Chiel92/vim-autoformat
+git clone https://github.com/fatih/vim-go
